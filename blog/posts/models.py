4aed8cb84357ae5bf8b5c18a1ec02a4df43af8d2
Romanovich Maxim <maximqq123@gmail.com>
2024-02-17 01:53:14 +0300

    Add author field to Post model, change feed.html

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,12 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from users.models import User
+
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=4000)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    # author = models.ForeignKey(to=User)
+    author = models.ForeignKey(to=User, on_delete=models.SET(get_sentinel_user))
 
     class Meta:
         db_table = 'post'
