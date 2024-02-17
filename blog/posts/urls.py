854d5f5ad0b8adfc441e5f284591d92decdcf083
Romanovich Maxim <maximqq123@gmail.com>
2024-02-17 23:30:19 +0300

    Add viewing posts by a specific user

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from posts.views import FeedView, PostView
+from posts.views import FeedView, PostView, AuthorView
 
 urlpatterns = [
     path('feed/', FeedView.as_view(), name='feed'),
     path('post/<int:pk>', PostView.as_view(), name='post'),
+    path('author/<slug:username>/', AuthorView.as_view(), name='author'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from posts.models import Post
+from users.models import User
 
 
 class FeedView(ListView):
@@ -16,3 +17,14 @@ class PostView(DetailView):
     model = Post
     template_name = 'posts/post.html'
     context_object_name = 'post'
+
+
+class AuthorView(ListView):
+    model = Post
+    template_name = 'posts/feed.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        author = User.objects.get(username=self.kwargs['username'])
+        queryset = Post.objects.filter(author=author)
+        return queryset
