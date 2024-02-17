71a103c17260dfac5badc698feea52bca9d73d71
Romanovich Maxim <maximqq123@gmail.com>
2024-02-17 23:48:37 +0300

    Add search by post title and text

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from posts.views import FeedView, PostView, AuthorView
+from posts.views import FeedView, PostView, AuthorView, SearchView
 
 urlpatterns = [
     path('feed/', FeedView.as_view(), name='feed'),
     path('post/<int:pk>', PostView.as_view(), name='post'),
     path('author/<slug:username>/', AuthorView.as_view(), name='author'),
+    path('search/', SearchView.as_view(), name='search'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from posts.models import Post
@@ -27,4 +28,16 @@ class AuthorView(ListView):
     def get_queryset(self):
         author = User.objects.get(username=self.kwargs['username'])
         queryset = Post.objects.filter(author=author)
+        return queryset
+
+
+class SearchView(ListView):
+    model = Post
+    template_name = 'posts/feed.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        search_vector = SearchVector('title', 'content')
+        queryset = Post.objects.annotate(search=search_vector).filter(search=query)
         return queryset
